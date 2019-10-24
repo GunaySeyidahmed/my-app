86476aa1deba1e12d47c832bfee12a3abd72112f
Gunay <gu.mammadova@expressbank.az>
2019-10-24 13:57:12 +0400

    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  
+  state = {
+    count: 0
+  }
+
+  handleIncrement = (test) => { 
+    console.log("test",test)
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleIncrement}>Increment</button>
+      </div>
+    )
+  }
+
 }
 
 export default App;
+
